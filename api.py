6188236857b074d3f12ed32431eb808c5a8ade72
Samuel França <samuelffn@gmail.com>
2020-11-20 00:22:54 -0300

    Update api.py

--- api.py
+++ api.py
@@ -14,6 +14,6 @@ def rota(nome):
 
 app.run(host="0.0.0.0", port= 2000, debug= False)
 # host="0.0.0.0" permite que outra máquina ou dispositivo acesse a aplicação, sem ser localhost
-# port= 2000 é a porta onde a aplicação vai rodar
+# port= 2000 é a porta onde a aplicação vai rodar. Por padrão roda na porta 5000
 # debug= False para não seja feito debug
 
