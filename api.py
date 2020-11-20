0299edd4f7f7bf573f46f5d3efa117a4dd4f4662
samuelffn <samuelffn@gmail.com>
2020-11-20 01:22:22 -0300

    Atualizando anotaçoes de informaçoes

--- api.py
+++ api.py
@@ -13,7 +13,14 @@ def rota(nome):
     return "Meu nome é " + nome
 
 app.run(host="0.0.0.0", port= 2000, debug= False)
+## Informações sobre os parâmetros utilizados:
 # host="0.0.0.0" permite que outra máquina ou dispositivo acesse a aplicação, sem ser localhost
 # port= 2000 é a porta onde a aplicação vai rodar. Por padrão roda na porta 5000
 # debug= False para não seja feito debug
 
+
+##  Instruções para execução deste arquivo:
+# 1- No terminal acessa o diretório do projeto  
+# 2- Utiliza o comando **python3 api.py**  
+# 3- Abra o navegador para testar a rota padrão: http://localhost:2000/  
+# 4- Teste a outra rota utilizando parâmetro: http://localhost:2000/rota-nome/Samuel
