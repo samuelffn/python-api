88bd851e3e74811fe57b9d81cc4bc1addf01af77
samuelffn <samuelffn@gmail.com>
2020-11-20 01:05:27 -0300

    Fazendo requisiçoes com tratamento de mensagens

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from usuario_controller import insertUsuario
 
 app = Flask("python-api")
 
@@ -8,8 +9,31 @@ def olaMundo():
 
 @app.route("/cadastra/usuario", methods=["POST"])
 def cadastraUsuario():
+    
     body = request.get_json()
-    print(body)
-    return body
+
+    if("nome" not in body):
+        return geraResponse(400, "O campo nome é obrigatório")
+
+    if("email" not in body):
+        return geraResponse(400, "O campo email é obrigatório")
+
+    if("senha" not in body):
+        return geraResponse(400, "O campo senha é obrigatório")
+
+    usuario = insertUsuario(body["nome"], body["email"], body["senha"])
+
+    return geraResponse(200, "Usuario criado", "user",  usuario)
+
+def geraResponse(status, mensagem, nome_do_conteudo=False, conteudo=False):
+    tipoMensagem = "mensagem" if status == 200 else "erro"
+    response = {}
+    response["status"] = status
+    response[tipoMensagem] = mensagem
+
+    if(nome_do_conteudo and conteudo):
+        response[nome_do_conteudo] = conteudo
+
+    return response
 
 app.run(host="0.0.0.0", port= 2000, debug= False)
---
+++ usuario_controller.py
@@ -0,0 +1,5 @@
+def insertUsuario(nome, email, senha):
+
+    # Aqui viria toda regra para salvar no banco de dados
+
+    return { "id": 1, "nome": nome, "senha": senha}
